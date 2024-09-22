37c80413e812695db49dfc42c511c70bd7474d5b
p3chaeyeon <p3chaeyeon@gmail.com>
2024-09-22 18:33:41 +0900

    Settings

---
+++ src/main/java/user/bean/UserDTO.java
@@ -0,0 +1,15 @@
+// FilmNote/src/main/java/user.bean.UserDTO.java
+package user.bean;
+
+import java.util.Date;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class UserDTO {
+
+}
