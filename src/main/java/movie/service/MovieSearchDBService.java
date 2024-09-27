0915298734a69ace7fca02a94b3a9bc89c0187a7
p3chaeyeon <p3chaeyeon@gmail.com>
2024-09-27 14:16:30 +0900

    게시판 검색 구현 토스 준비

--- src/main/java/movie/service/MovieSearchDBService.java
+++ src/main/java/movie/service/MovieSearchDBService.java
@@ -11,7 +11,7 @@ public class MovieSearchDBService implements CommandProcess {
 	@Override
 	public String requestPro(HttpServletRequest request, HttpServletResponse response) throws Throwable {
 		// TODO Auto-generated method stub
-		return null;
+		return "none";
 	}
 
 }
